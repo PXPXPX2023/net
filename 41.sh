58430b0081633d756347a636d97934b3f3af86e9
PXPXPX2023 <123269704+PXPXPX2023@users.noreply.github.com>
2026-04-02 00:19:10 +0800

    Modify network settings in 41.sh
    Updated various TCP and network parameters in 41.sh, including changes to memory settings and timeout values.

--- 41.sh
+++ 41.sh
@@ -57,25 +57,30 @@ net.ipv4.conf.default.rp_filter = 0
 net.ipv4.tcp_no_metrics_save = 1
 net.ipv4.tcp_ecn = 1
 net.ipv4.tcp_ecn_fallback = 1
-net.ipv4.tcp_frto = 2
-net.ipv4.tcp_low_latency = 1
+
+#net.ipv4.tcp_frto = 2
+#net.ipv4.tcp_low_latency = 1
+
 net.ipv4.tcp_mtu_probing = 1
 net.ipv4.tcp_rfc1337 = 1
 net.ipv4.tcp_sack = 1
-net.ipv4.tcp_fack = 1
+
+#net.ipv4.tcp_fack = 1
+
 net.ipv4.tcp_window_scaling = 1
 #窗口
 net.ipv4.tcp_adv_win_scale = 1
 net.ipv4.tcp_moderate_rcvbuf = 1
-net.core.rmem_default = 5260997
-net.core.wmem_default = 5260997
-net.core.rmem_max = 95108858
-net.core.wmem_max = 95108858
+net.core.rmem_default = 6260997
+net.core.wmem_default = 6260997
+net.core.rmem_max = 96108858
+net.core.wmem_max = 96108858
 net.core.netdev_budget = 600
-net.ipv4.fib_sync_mem = 6666354
+
+#net.ipv4.fib_sync_mem = 6666354
 net.ipv4.igmp_max_memberships = 200
-net.ipv4.tcp_rmem = 4096 5260997 95108858
-net.ipv4.tcp_wmem = 4096 5260997 95108858
+net.ipv4.tcp_rmem = 4096 6260997 96108858
+net.ipv4.tcp_wmem = 4096 6260997 96108858
 net.ipv4.udp_rmem_min = 8192
 net.ipv4.udp_wmem_min = 8192
 net.ipv4.route.flush = 1
@@ -85,22 +90,24 @@ fs.file-max = 1048576
 fs.nr_open = 1048576
 fs.inotify.max_user_instances = 262144
 net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_fin_timeout = 60
+net.ipv4.tcp_fin_timeout = 12
 net.ipv4.tcp_keepalive_probes = 5
-net.ipv4.tcp_keepalive_time = 600
+net.ipv4.tcp_keepalive_time = 300
 net.ipv4.tcp_keepalive_intvl = 30
 net.ipv4.tcp_tw_reuse = 1
 net.ipv4.tcp_abort_on_overflow = 0
 net.ipv4.ip_local_port_range = 1024 65535
 net.ipv4.tcp_max_syn_backlog = 65535
 net.ipv4.tcp_max_tw_buckets = 262144
-net.ipv4.route.gc_timeout = 1350
+
+#net.ipv4.route.gc_timeout = 1350
+
 net.ipv4.tcp_retries1 = 5
 net.ipv4.tcp_retries2 = 8
 net.ipv4.tcp_syn_retries = 3
 net.ipv4.tcp_synack_retries = 2
 net.core.somaxconn = 65535
-net.core.netdev_max_backlog = 8192
+net.core.netdev_max_backlog = 65535
 net.ipv4.tcp_timestamps = 1
 net.ipv4.tcp_max_orphans = 262144
 net.core.optmem_max = 3276800
@@ -108,7 +115,7 @@ net.ipv4.tcp_fastopen = 3
 net.ipv4.tcp_reordering = 3
 
 net.ipv4.tcp_autocorking = 1
-net.ipv4.tcp_tso_win_divisor = 68
+net.ipv4.tcp_tso_win_divisor = 80
 kernel.pid_max = 4194304
 kernel.threads-max = 75536
 net.ipv4.neigh.default.gc_thresh1 = 1024
@@ -119,10 +126,12 @@ net.ipv4.conf.default.arp_announce = 2
 net.ipv4.conf.lo.arp_announce = 2
 net.ipv4.conf.all.arp_announce = 2
 net.unix.max_dgram_qlen = 130000
-net.ipv4.tcp_notsent_lowat = 30005
-vm.min_free_kbytes = 65535
-net.ipv4.route.max_size = 655350
-vm.vfs_cache_pressure = 2
+net.ipv4.tcp_notsent_lowat = 40005
+
+#vm.min_free_kbytes = 65535
+
+#net.ipv4.route.max_size = 655350
+vm.vfs_cache_pressure = 10
 net.ipv4.conf.all.send_redirects = 0
 net.ipv4.conf.all.accept_redirects = 0
 net.ipv4.conf.all.accept_source_route = 0
@@ -167,7 +176,7 @@ net.core.flow_limit_table_len = 131072
 
 net.ipv4.tcp_workaround_signed_windows = 1
 vm.dirty_ratio = 35
-vm.overcommit_memory = 1
+vm.overcommit_memory = 0
 kernel.sysrq = 1
 net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
@@ -177,7 +186,7 @@ net.ipv4.tcp_child_ehash_entries = 65535
 
 net.ipv4.ip_no_pmtu_disc = 0
 net.ipv4.tcp_stdurg = 0
-net.ipv4.tcp_challenge_ack_limit = 6
+net.ipv4.tcp_challenge_ack_limit = 1200
 net.ipv4.tcp_comp_sack_delay_ns = 700000
 net.ipv4.tcp_comp_sack_nr = 2
 net.ipv4.tcp_fwmark_accept = 1
@@ -209,7 +218,9 @@ net.ipv4.tcp_min_tso_segs = 1
 net.ipv4.tcp_thin_linear_timeouts = 0
 net.ipv4.tcp_early_demux = 1
 net.ipv4.tcp_shrink_window = 0
-net.ipv4.route.gc_min_interval_ms = 700
+
+#net.ipv4.route.gc_min_interval_ms = 700
+
 net.ipv4.neigh.default.unres_qlen_bytes = 65535
 
 #net.ipv4.tcp_retrans_collapse = 3 #老旧废弃
@@ -223,4 +234,3 @@ echo "应用 sysctl 参数2..."
 sudo sysctl -p
 
 reboot
-
