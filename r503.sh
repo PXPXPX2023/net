7f24648218a6050e8d5bc60bbed8005c1ba76be1
PXPXPX2023 <123269704+PXPXPX2023@users.noreply.github.com>
2026-04-08 15:08:12 +0800

    Update r503.sh

--- r503.sh
+++ r503.sh
@@ -192,8 +192,8 @@ fs.protected_hardlinks = 1
 fs.protected_regular = 2
 fs.protected_symlinks = 1
 
-net.core.rps_sock_flow_entries = 65535
-net.core.flow_limit_table_len = 131072
+net.core.rps_sock_flow_entries = 32768
+net.core.flow_limit_table_len = 65536
 
 net.ipv4.tcp_workaround_signed_windows = 1
 vm.dirty_ratio = 35
