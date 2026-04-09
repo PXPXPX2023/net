c307860421b8cf531a01e9b4ecb0842bf14ac036
PXPXPX2023 <123269704+PXPXPX2023@users.noreply.github.com>
2026-04-09 19:17:22 +0800

    Create if6.sh

---
+++ if6.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# if6.sh — 这个文件用 sh 写，兼容所有系统
+set -e
+REPO="https://raw.githubusercontent.com/PXPXPX2023/net/refs/heads/main"
+curl -sS -o /root/xrayv6f6.sh "$REPO/xrayv6f6.sh"
+chmod +x /root/xrayv6f6.sh
+ln -sf /root/xrayv6.sh /usr/local/bin/xrv
+echo "安装完成，输入 xrv 启动"
